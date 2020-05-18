95d5fae2690b58ca3ae1b165419a324ecdb18554
Matt Watson <mattwat@gmail.com>
2020-05-18 15:29:41 -0400

    omit page links for lots of search results

--- src/components/search/pagination-tray.js
+++ src/components/search/pagination-tray.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Button } from '../buttons'
-import { ChevronLeftIcon, ChevronRightIcon, FirstPageIcon, LastPageIcon } from '../icons'
+import { ChevronLeftIcon, ChevronRightIcon, FirstPageIcon, LastPageIcon, MoreHorizontalIcon } from '../icons'
 
 const Wrapper = styled.nav`
     display: flex;
     justify-content: center;
+    align-items: stretch;
     margin-bottom: 3rem;
     ${ Button } {
         margin: 0.5rem 0.25rem;
@@ -17,6 +18,14 @@ const Wrapper = styled.nav`
     }
 `
 
+const Ellipsis = () => {
+    return (
+        <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '0 0.5rem' }}>
+            <MoreHorizontalIcon fill="var(--color-crimson)" size={ 24 } />
+        </span>
+    )
+}
+
 export const PaginationTray = ({ currentPage, links, prevPageHandler, nextPageHandler, firstPageHandler, lastPageHandler }) => {
     return (
         <Wrapper role="navigation" aria-label="Pagination Navigation">
@@ -26,9 +35,10 @@ export const PaginationTray = ({ currentPage, links, prevPageHandler, nextPageHa
             <Button aria-label={ `Go to previous page` } light disabled={ prevPageHandler === null } onClick={ prevPageHandler } style={{ padding: 0 }}>
                 <ChevronLeftIcon fill="var(--color-crimson)" size={ 24 } />
             </Button>
+            { currentPage >= 4 && <Ellipsis /> }
             {
                 links.map((link, i) => (
-                    <Button
+                    Math.abs(currentPage - i) <= 3 && <Button
                         key={ `page-${ i }` }
                         aria-label={ i === currentPage ? `Page ${ i + 1 }, Current page` : `Go to page ${ i + 1 }` }
                         aria-current={ i === currentPage }
@@ -39,6 +49,7 @@ export const PaginationTray = ({ currentPage, links, prevPageHandler, nextPageHa
                     </Button>
                 ))
             }
+            { currentPage < links.length - 4 && <Ellipsis /> }
             <Button aria-label={ `Go to next page` } light disabled={ nextPageHandler === null } onClick={ nextPageHandler } style={{ padding: 0 }}>
                 <ChevronRightIcon fill="var(--color-crimson)" size={ 24 } />
             </Button>
